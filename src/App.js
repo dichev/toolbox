d0f92820c540d57c8c9aef3ac631d1eb00e346fb
dichev <g.dichev@gmail.com>
2018-06-29 13:18:06 +0300

    Change loop syntax

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App {
     constructor() {
         this.params = {}
         this._pools = []
+        this._loopBy = null
         
         program
             .option('-p, --parallel [limit]', 'When run with multiple hosts define how many commands to be executed in parallel. Set to 0 execute them all together. By default will be executed sequentially')
@@ -33,70 +34,64 @@ class App {
     option(flags, description = '', { def, choices } = {}){
         program.option(flags, description, (val) => {
             if(!choices || !Array.isArray(choices) || !choices.length) return
-            if(val === 'all') return choices.slice()
-            let values = val.split(',')
-            values.forEach(v => {
+            if(val === 'all') return choices.join(',')
+            val.split(',').forEach(v => {
                 if(!choices.includes(v)) throw Error(`There is no such choice ${v}. Available: ${choices}`)
             })
-            return values
+            return val
         }, def)
         
         return this
     }
     
     /**
-     * Alias of once method
+     * @param {string}   option - specify by which option to loop, normally is 'hosts'
+     * @return this
+     */
+    loop(option = 'hosts'){
+        this._loopBy = option
+        return this
+    }
+    
+   
+    /**
      * @param {function} fn
      * @return this
      */
     async run(fn) {
-        return this.once(fn)
-    }
-    
-    /**
-     * @param {function} fn
-     * @return this
-     */
-    async once(fn){
-        try {
-            program.parse(process.argv)
-            this.params = program // TODO: temporary, we should expose only the parsed arguments, see console.log(program)
-            await fn()
-        }
-        catch (err) {
-            this._errorHandler(err)
-        }
-        this.destroy()
-    
-        return this
-    }
-    
-    /**
-     * @param {string}   option - specify by which option to loop, normally is 'hosts'
-     * @param {function} fn
-     * @return this
-     */
-    async loop(option = 'hosts', fn) {
         try {
             program.parse(process.argv)
             this.params = program // TODO: temporary, we should expose only the parsed arguments, see console.log(program)
     
-            if (!option || !fn || typeof option !== 'string' || typeof fn !== 'function') {
-                throw Error(`Invalid arguments! Expected deployer.loop(string, function), received deployer.loop(${typeof option}, ${typeof fn})`)
-            }
-            if(!this.params[option] || !Array.isArray(this.params[option])){
-                throw Error(`Invalid paramater option:(${option})! It's expected to be array and to be predefined as cli option`)
+            let HOSTS = null
+            
+            if (typeof fn !== 'function') {
+                throw Error(`Invalid arguments! Expected deployer.run(async function), received deployer.run(${typeof fn})`)
             }
             if (this.params.parallel !== undefined) {
                 throw Error(`TODO: support parallelizm`)
             }
             
-            let HOSTS = this.params[option]
-            for(let host of HOSTS) {
-                if(HOSTS.length > 1) {
-                    console.log(`\n-- ${host} -----------------------------------------`)
+            if(this._loopBy) {
+                let param = this.params[this._loopBy]
+                if(!param){
+                    throw Error(`Invalid paramater option:(${this._loopBy})! It's expected to be array and to be predefined as cli option`)
                 }
-                await fn(host)
+    
+                HOSTS = param.split(',')
+            }
+            
+            
+            if(!HOSTS){
+                await fn()
+            }
+            else {
+                for (let host of HOSTS) {
+                    if (HOSTS.length > 1) {
+                        console.log(`\n-- ${host} -----------------------------------------`)
+                    }
+                    await fn(host)
+                }
             }
         }
         catch (err) {
