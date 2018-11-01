7304d711d2d2661e42acd3293abe0e364ce0a0df
dichev <g.dichev@gmail.com>
2018-11-01 09:23:36 +0200

    Improve tester readability

--- src/tools/Tester.js
+++ src/tools/Tester.js
@@ -12,6 +12,24 @@ let report = {pass: 0, fail: 0, warn: 0}
  */
 class Tester {
     
+    /**
+     * @return {{fail: number, pass: number}}
+     */
+    static report(errorOnFail = true) {
+        let {pass, fail, warn} = report
+    
+        console.log(`\n\n----------------------------------------------`)
+    
+        if (pass) console.log(colors.green(`Passed ${pass} test cases`))
+        if (warn) console.log(colors.yellow(`Warning on ${warn} test cases`))
+        if (fail) {
+            if (errorOnFail) throw Error(`Failed ${fail} test cases`)
+            console.log(colors.red(`Failed ${fail} test cases`))
+            console.log(colors.gray('Run --verbose mode to see the errors stack'))
+        }
+        return report
+    }
+    
     constructor(prefix = '', silent = false){
         this.testCases = []
         
@@ -28,10 +46,10 @@ class Tester {
         this.it.warn = this.warn.bind(this)
     }
     
-    async run(errorOnFail = true){
+    async run(errorOnFail = true, suitName = ''){
         if(this.isRunning) return // could happen in parallel execution
         this.isRunning = true
-        if(!this.silent) console.log(`\n\n--- Running the test suit ---------------`)
+        if(!this.silent) console.log(`\n--- Running test suit: ${suitName} ---------------`)
     
         let pass = 0
         let fail = 0
@@ -47,22 +65,22 @@ class Tester {
                 else {
                     await fn()
                     if(this._skipped) {
-                        console.info(colors.gray(`  [-] ${title}`))
+                        console.log(colors.gray(`  [-] ${title}`))
                         this._skipped = false
                     } else {
-                        console.info(colors.green(`  [√] ${title}`))
+                        console.log(colors.green(`  [√] `) + title)
                     }
                 }
                 pass++
             } catch (err) {
                 if(type === 'warn'){
-                    console.warn(colors.yellow(`  [!] ${title}`))
+                    console.warn(colors.yellow(`  [!] `) + title)
                     warn++
                 } else {
                     console.error(colors.red(`  [x] ${title}`))
                     fail++
                 }
-                console.log(colors.gray('      ' + err.message || err.toString()))
+                console.log(colors.gray(err.message || err.toString()))
                 if(verbose && err.stack) console.log('      ' + colors.gray(err.stack.replace(/\r?\n/g, '\n      ') + '\r\n'))
             }
         }
@@ -70,9 +88,9 @@ class Tester {
         report.pass += pass
         report.fail += fail
         report.warn += warn
-        if(errorOnFail) this.status()
+        if(errorOnFail) this.status(true)
     
-        if (!this.silent) console.log(`----------------------------------------------\n\n`)
+        // if (!this.silent) console.log(`----------------------------------------------\n`)
         this.isRunning = false
         return { fail, pass, warn }
     }
@@ -81,16 +99,7 @@ class Tester {
      * @return {{fail: number, pass: number}}
      */
     status(errorOnFail = true){
-        let {pass, fail, warn} = report
-        
-        if (pass) console.log(colors.green(`Passed ${pass} test cases`))
-        if (warn) console.log(colors.yellow(`Warning on ${warn} test cases`))
-        if (fail) {
-            if (errorOnFail) throw Error(`Failed ${fail} test cases`)
-            console.log(colors.red(`Failed ${fail} test cases`))
-            console.log(colors.gray('Run --verbose mode to see the errors stack'))
-        }
-        return report
+        return Tester.report()
     }
     
     
