3d4ddb18cfe2ebd3bdbc66c9267d6e0c3f064306
Luka Bozhich <l.bozhich@dopamine.bg>
2019-05-23 18:23:22 +0300

    add askMultiline, expecting multiline input, edning with ; remove all ` symbols from inputs - bugging bash

--- src/tools/Input.js
+++ src/tools/Input.js
@@ -21,6 +21,7 @@ class Input {
             })
             
             rl.question(question, (answer) => {
+                answer = answer.replace(new RegExp(/`/, 'g'), '')
                 rl.close()
                 
                 answer = (answer || def).toLowerCase().trim()
@@ -88,6 +89,60 @@ class Input {
         })
     }
     
+    async askMultiline(question, choices, def){
+        return new Promise((resolve, reject) => {
+            
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout,
+                terminal: true, // when the readline is called from child process, then rl.write doesn't display the written characters without this option
+            })
+            
+            let msg = question
+            if (choices && choices.length) {
+                msg += ` (${choices.join(',')}): `
+            } else {
+                msg += `: `
+            }
+            
+            let listener
+            if (this.history) {
+                listener = (s, key) => {
+                    if (key.name === 'up' || key.name === 'down') {
+                        rl.write(null, {ctrl: true, name: 'u'}); // this will clear the current input
+                        let cmd = key.name === 'up' ? this.history.prev() : this.history.next()
+                        rl.write(cmd)
+                    }
+                }
+                process.stdin.on('keypress', listener)
+            }
+            
+            rl.prompt(msg)
+            let input = []
+            rl.on('line', function (cmd) {
+                cmd = cmd.replace(new RegExp(/`/, 'g'), '')
+                input.push(cmd.trim())
+                if (cmd === ';' || ~cmd.indexOf(';')) rl.close()
+            });
+            
+            rl.on('close', () => {
+                if(this.history) this.history.add(input.join(' '))
+                resolve(input.join(' '))
+            });
+            
+            rl.on('SIGINT', () => {
+                rl.clearLine(0)
+                rl.clearLine(0)
+                rl.question('Ctrl+C recieved! Are you sure you want to exit? ', (answer) => {
+                    if (answer.match(/^y(es)?$/i)) reject('User cancel');
+                });
+            });
+            
+            if (choices && def) {
+                rl.write(def)
+            }
+        })
+    }
 }
 
 module.exports = Input
