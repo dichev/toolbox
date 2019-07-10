73c2c01c42c9cc60d2ada387c2415c6b8d7e7547
plamen <p.yonchev@dopamine.bg>
2019-07-10 13:42:41 +0300

    Order fields + remove environment field

--- .db/deploy_log.sql
+++ .db/deploy_log.sql
@@ -5,12 +5,11 @@ USE `deploy`;
 CREATE TABLE IF NOT EXISTS `deploy_log` (
 `id` int(1) UNSIGNED NOT NULL AUTO_INCREMENT,
 `action` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-`environment` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
 `user` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-`message` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-`debugInfo` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
 `jiraTicketId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
 `status` enum('IN_PROGRESS','SUCCESS','ERROR','ABORT') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`message` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+`debugInfo` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
 `startAt` datetime(0) NULL DEFAULT NULL,
 `endAt` datetime(0) NULL DEFAULT NULL,
 PRIMARY KEY (`id`) USING BTREE
