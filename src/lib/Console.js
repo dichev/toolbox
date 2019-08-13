3190f4066f8cdb59583c83040addee57ba4a19c4
dichev <g.dichev@gmail.com>
2019-08-13 09:15:58 +0300

    Console: emit events for warnings & errors

--- src/lib/Console.js
+++ src/lib/Console.js
@@ -2,6 +2,8 @@
 
 //TODO: rename to Console
 const colors = require('chalk')
+const stripAnsi = require('strip-ansi')
+const EventEmitter = require('events').EventEmitter
 
 let verbose = (process.argv.findIndex(arg => arg === '-v' || arg === '--verbose') !== -1)
 
@@ -12,6 +14,9 @@ const _console = {
     error: console.error
 }
 
+/** @type module:events.internal.EventEmitter **/
+let _emitter = null
+
 class Console {
     
     static verbose(...args){
@@ -19,7 +24,6 @@ class Console {
             _console.log.apply(_console, args.map(a => colors.gray(a)))
         }
     }
-    
     static log(...args){
         _console.log.apply(_console, args)
     }
@@ -28,18 +32,46 @@ class Console {
     }
     static warn(...args){
         _console.warn.apply(_console, args.map(a => colors.yellow(a)))
+        Console._emit('warn', ...args)
     }
     static error(...args){
         _console.error.apply(_console, args.map(a => colors.red(a)))
+        Console._emit('error', ...args)
     }
     
+    
+    static on(eventName, listener){
+        if(!_emitter) return this.warn('console.on() - The console.upgrade() is NOT invoked')
+        return _emitter.on(eventName, listener)
+    }
+    static once(eventName, listener){
+        if (!_emitter) return this.warn('console.on() - The console.upgrade() is NOT invoked')
+        return _emitter.once(eventName, listener)
+    }
+    static off(eventName, listener){
+        if (!_emitter) return this.warn('console.on() - The console.upgrade() is NOT invoked')
+        return _emitter.off(eventName, listener)
+    }
+    static _emit(eventName, ...args){
+        if(!_emitter) return
+        return _emitter.emit(eventName, ...args.map(stripAnsi))
+    }
+    
+    
     static upgrade(){
+        if(this._upgraded) return this.warn('The console.upgrade() is already invoked! Please check for wrong code flow..')
+        this._upgraded = true
+
+        // do not bind Console here (to preserve the stack tree)
         console.verbose = this.verbose
         console.log = this.log
         console.info = this.info
         console.warn = this.warn
         console.error = this.error
+    
+        _emitter = new EventEmitter()
     }
+    
 }
 
 // just map all other console functions for compatibility
