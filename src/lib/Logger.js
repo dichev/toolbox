499a0272ad8cdf26b8410b0fe5e359fcc769eec3
plamen <p.yonchev@dopamine.bg>
2019-08-15 14:59:36 +0300

    Add grafana annotations when logging.

--- src/lib/Logger.js
+++ src/lib/Logger.js
@@ -2,6 +2,7 @@
 
 const MySQLClient = require('../tools/MySQL')
 const SSHClient = require('../tools/SSHClient')
+const fetch = require('node-fetch')
 
 // this._config = {
 //    "mysql": {
@@ -10,6 +11,10 @@ const SSHClient = require('../tools/SSHClient')
 //        "password": "",
 //        "database": "envs",
 //        "ssh": false
+//    },
+//    grafana: { // Allows adding of annotations with curl request
+//        "apiUrl": false, // https://prodmon.dopamine.bg/api/annotations
+//        "apiKey": false  // You can get the api key from here https://prodmon.dopamine.bg/org/apikeys
 //    }
 // }
 class Logger {
@@ -29,8 +34,7 @@ class Logger {
         if (!this.enabled) return
 
         await this._prepare()
-
-        await this._log(info)
+        await this._log(info, 'start')
     }
 
     /**
@@ -48,15 +52,16 @@ class Logger {
             endAt: this._getDateTime()
         }
 
-        await this._log(info)
+        await this._log(info, 'end')
         await this._destroy()
     }
 
     /**
      * @param {object} info
+     * @param {string} source
      * @private
      */
-    async _log(info){
+    async _log(info, source){
         if (!this._db) console.log( 'Logger: No db initialised!' )
 
         // Write to mySQL log
@@ -69,7 +74,49 @@ class Logger {
                 console.log(e)
             }
         }
+
+        await this._logGrafanaAnnotation(source, info);
+
         return this._dbRecordId
+    }
+
+    async _logGrafanaAnnotation(source, info) {
+        if (source === 'start' && this._config.grafana.apiUrl && this._config.grafana.apiKey) {
+            try {
+                let debugInfo = JSON.parse(info.debugInfo)
+
+                let options = {
+                    url: this._config.grafana.apiUrl,
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': `Bearer ${this._config.grafana.apiKey}`
+                    },
+                    body: {
+                        time: Date.now(),
+                        isRegion: true,
+                        timeEnd: Date.now(),
+                        text: "Deploy: " + (debugInfo.rev !== undefined ? debugInfo.rev : "-"),
+                        data: debugInfo,
+                        tags: ["deploy"],
+                    }
+                }
+
+                if (info.action.includes('cdn/')) {
+                    options.body.tags.push("frontend")
+                } else if (info.action.includes('hermes/')) {
+                    options.body.tags.push("backend")
+                } else {}
+                // Stringify body
+                options.body = JSON.stringify(options.body)
+
+                process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+                await fetch(this._config.grafana.apiUrl, options)
+                process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
+
+            } catch (e) {}
+        }
     }
 
     /**
@@ -102,7 +149,7 @@ class Logger {
     }
 
     /**
-     * @returns {Date}
+     * @returns {string}
      * @private
      */
     _getDateTime(){
